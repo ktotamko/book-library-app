98c259e381a7e506d0f86077c778395ea50eb153
ktotamko <ktotamko@gmail.com>
2024-11-29 16:50:46 +0000

    Toggle favorite book

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { deleteBook } from '../../redux/books/actionCreators'
+import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
+import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
 import './BookList.css'
 
 const BookList = () => {
@@ -9,6 +10,10 @@ const BookList = () => {
 
 	const handleDeleteBook = id => {
 		dispatch(deleteBook(id))
+	}
+
+	const handleToggleFavorite = id => {
+		dispatch(toggleFavorite(id))
 	}
 
 	return (
@@ -24,7 +29,16 @@ const BookList = () => {
 								{++i}. {book.title} by <strong>{book.author}</strong>
 							</div>
 							<div className='book-actions'>
-								<button onClick={() => handleDeleteBook(book.id)}>Delete</button>
+								<span onClick={() => handleToggleFavorite(book.id)}>
+									{book.isFavorite ? (
+										<BsBookmarkStarFill className='star-icon' />
+									) : (
+										<BsBookmarkStar className='star-icon' />
+									)}
+								</span>
+								<button onClick={() => handleDeleteBook(book.id)}>
+									Delete
+								</button>
 							</div>
 						</li>
 					))}
