0a0f13f6c1815b7bec079af494d6bbb3ffa9159f
ktotamko <ktotamko@gmail.com>
2024-12-11 11:25:58 +0000

    Add book source

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -15,7 +15,7 @@ const BookForm = () => {
 		e.preventDefault()
 
 		if (title && author) {
-			dispatch(addBook(createBookWithId({ title, author })))
+			dispatch(addBook(createBookWithId({ title, author }, 'manual')))
 			setTitle('')
 			setAuthor('')
 		}
@@ -24,16 +24,14 @@ const BookForm = () => {
 	const handleAddRandomBook = () => {
 		const randomIndex = Math.floor(Math.random() * booksData.length)
 		const randomBook = booksData[randomIndex]
-		const randomBookWithId = createBookWithId(randomBook)
-
-		dispatch(addBook(randomBookWithId))
+		dispatch(addBook(createBookWithId(randomBook, 'random')))
 	}
 
 	const handleAddRandomBookViaAPI = async () => {
 		try {
 			const res = await axios.get('http://localhost:4000/random-book')
 			if (res?.data?.title && res?.data?.author) {
-				dispatch(addBook(createBookWithId(res.data)))
+				dispatch(addBook(createBookWithId(res.data, 'API')))
 			}
 		} catch (error) {
 			console.log('Error fetching random book:', error)
--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -66,7 +66,8 @@ const BookList = () => {
 						<li key={book.id}>
 							<div className='book-info'>
 								{++i}. {highlightMatches(book.title, titleFilter)} by{' '}
-								<strong>{highlightMatches(book.author, authorFilter)}</strong>
+								<strong>{highlightMatches(book.author, authorFilter)}</strong> (
+								{book.source})
 							</div>
 							<div className='book-actions'>
 								<span onClick={() => handleToggleFavorite(book.id)}>
